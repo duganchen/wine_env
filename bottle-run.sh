e87717415e68469ff29370fe5ae5d9ae634f8a93
Dugan Chen <thedoogster@gmail.com>
2014-11-23 15:12:21 -0800

    spaces-tab inconsistency fixed

--- bottle-run.sh
+++ bottle-run.sh
@@ -12,8 +12,8 @@ fi
 # Will deal with this repetition later.
 
 if [ ! -d ~/.local/share/wineprefixes/$1 ]; then
-	echo "$1 is not a bottle."
-	echo Usage: bottle-run bottle command \[args\]
+    echo "$1 is not a bottle."
+    echo Usage: bottle-run bottle command \[args\]
     echo E.g. bottle-run PvZ winecfg
     echo e.g. bottle-run PvZ wine notepad
     echo e.g. bottle-run PvZ wine iexplore http://www.google.ca/
@@ -25,13 +25,13 @@ RUNRC=~/.local/share/wineprefixes/$1/bin/runrc
 
 if [ ! -f $RUNRC ];  then
     echo "$RUNRC not found."
-	echo "Please use mkbottle to recreate the bottle."
+    echo "Please use mkbottle to recreate the bottle."
     exit 1
 fi
 
 # If we already have an environment loaded, clear it out.
 if [ -f "$WINEPREFIX/bin/uncorkrc" ]; then
-	. "$WINEPREFIX/bin/uncorkrc"
+    . "$WINEPREFIX/bin/uncorkrc"
 fi
 
 . ~/.local/share/wineprefixes/$1/bin/runrc
