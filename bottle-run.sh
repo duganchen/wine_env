382602f3bdae3f8c22dbaa0085693151a0631d52
Dugan Chen <thedoogster@gmail.com>
2014-11-24 07:04:19 -0800

    bottle-run.sh is cleaned up

--- bottle-run.sh
+++ bottle-run.sh
@@ -2,23 +2,28 @@
 
 
 if [ "$#" -lt 2 ]; then
-    echo Usage: bottle-run bottle command \[args\]
-    echo E.g. bottle-run PvZ winecfg
-    echo e.g. bottle-run PvZ wine notepad
-    echo e.g. bottle-run PvZ wine iexplore http://www.google.ca/
-    echo e.g. bottle-run PvZ winetricks --no-isolate steam
+	cat <<- EOF
+	Usage: bottle-run bottle command [args]
+	E.g. bottle-run PvZ winecfg
+	e.g. bottle-run PvZ wine notepad
+	e.g. bottle-run PvZ wine iexplore http://www.google.ca/
+	e.g. bottle-run PvZ winetricks --no-isolate steam
+	EOF
     exit 1
 fi
 
+BOTTLE=$1
 
-if [ ! -d "$HOME/.local/share/wineprefixes/$1" ]; then
-	echo "Bottle \"$1\" not found"
-	echo Run \"lsp\" to list bottles
-	echo \"bottle\" to create bottle
+if [ ! -d "$HOME/.local/share/wineprefixes/$BOTTLE" ]; then
+	cat <<- EOF
+	Bottle "$BOTTLE" not found"
+	Run "lsp" to list bottles
+	Run "bottle" to create bottles
+	EOF
 	exit 1
 fi
 
-RUNRC="$HOME/.local/share/wineprefixes/$1/bin/runrc.sh"
+RUNRC="$HOME/.local/share/wineprefixes/$BOTTLE/bin/runrc.sh"
 
 if [ ! -f "$RUNRC" ];  then
     echo "$RUNRC not found."
@@ -27,7 +32,7 @@ if [ ! -f "$RUNRC" ];  then
 fi
 
 # If we already have an environment loaded, clear it out.
-if [ -f "$WINEPREFIX/bin/uncorkrc.sh" ]; then
+if [ "$WINEPREFIX" != "" ] &&  [ -f "$WINEPREFIX/bin/uncorkrc.sh" ]; then
     . "$WINEPREFIX/bin/uncorkrc.sh"
 fi
 
