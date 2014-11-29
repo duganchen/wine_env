f44ce3131ece9d30920d3e3df1ced3e867dabf9e
Dugan Chen <thedoogster@gmail.com>
2014-11-29 13:49:28 -0800

    bottle-run has completion for the bottle

--- bottler.sh
+++ bottler.sh
@@ -57,14 +57,23 @@ lsp() {
 }
 
 
-_bottles() {
+_bottle_completion() {
 	COMPREPLY=()
 	if [ -d "$HOME/.local/share/wineprefixes" ]; then
-		COMPREPLY=( $( compgen -W "$( find $HOME/.local/share/wineprefixes -type d -mindepth 1 -maxdepth 1 -exec basename {} \; )" -- "${COMP_WORDS[COMP_CWORD]}"))
+		COMPREPLY=( $( compgen -W "$( find $HOME/.local/share/wineprefixes -type d -mindepth 1 -maxdepth 1 -exec basename {} \; )" -- "${COMP_WORDS[COMP_CWORD]}" ) )
+	fi
+}
+
+
+_runner_completion() {
+	COMPREPLY=()
+	if [ $COMP_CWORD = 1 ]; then
+		COMPREPLY=( $( compgen -W "$( find $HOME/.local/share/wineprefixes -type d -mindepth 1 -maxdepth 1 -exec basename {} \; )" -- "${COMP_WORDS[COMP_CWORD]}" ) )
 	fi
 }
 
 
 if [ -n "$BASH" ]; then
 	complete -F _bottles uncork
+	complete -F _runner_completion bottle-run
 fi
