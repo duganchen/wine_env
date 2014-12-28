01fa5fbc494f421414025a2981245cdd2a4a0322
Dugan Chen <thedoogster@gmail.com>
2014-12-27 23:31:06 -0800

    Shell detection is more reliable

--- wine_env_complete.sh
+++ wine_env_complete.sh
@@ -23,13 +23,13 @@ _zsh_completion() {
 	return 0
 }
 
-if [[ -n "$BASH" ]]; then
+if [[ -n "$BASH_VERSION" ]]; then
 	complete -F _bash_completion uncork
 	complete -F _bash_completion bottle-run
 	complete -F _bash_completion bottle
 fi
 
-if [[ -n "$ZSH" ]]; then
+if [[ -n "$ZSH_VERSION" ]]; then
 	compdef _zsh_completion uncork
 	compdef _zsh_completion bottle-run
 	compdef _zsh_completion bottle
