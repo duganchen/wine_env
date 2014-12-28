cdf8a3e53d8067c89edd63eedc0b4dd7b9a3e9b0
Dugan Chen <thedoogster@gmail.com>
2014-12-27 22:03:27 -0800

    setuptools is used

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
-from distutils.core import setup
-import itertools
-import os
-import shutil
-
-if not os.path.isdir('scripts'):
-    os.makedirs('scripts')
-
-raw_scripts = ('mkcorkrc.py', 'mkuncorkrc.py', 'mkrunrc.py', 'bottle-run.sh')
-all_scripts = [
-    os.path.join('scripts', os.path.splitext(script)[0]) for
-    script in raw_scripts
-]
-
-for src, dst in itertools.izip(raw_scripts, all_scripts):
-    shutil.copyfile(src, dst)
+from setuptools import setup, find_packages
 
 setup(
     name='wine_env',
     version='1.0',
-    py_modules=['wine_rc'],
-    scripts=all_scripts,
-    data_files=[('bin', ['bottler.sh', 'wine_env_complete.sh'])],
-    packages=['wine_env_powerline']
+    packages=find_packages(),
+    scripts=(
+        'mkcorkrc.py',
+        'mkuncorkrc.py',
+        'mkrunrc.py',
+        'bottle-run.sh',
+        'bottler.sh',
+        'wine_env_complete.sh'
+    )
 )
+# from distutils.core import setup
+# import itertools
+# import os
+# import shutil
+#
+# if not os.path.isdir('scripts'):
+#     os.makedirs('scripts')
+#
+# raw_scripts = ('mkcorkrc.py', 'mkuncorkrc.py', 'mkrunrc.py', 'bottle-run.sh')
+# all_scripts = [
+#     os.path.join('scripts', os.path.splitext(script)[0]) for
+#     script in raw_scripts
+# ]
+#
+# for src, dst in itertools.izip(raw_scripts, all_scripts):
+#     shutil.copyfile(src, dst)
+#
+# setup(
+#     name='wine_env',
+#     version='1.0',
+#     py_modules=['wine_rc'],
+#     scripts=all_scripts,
+#     data_files=[('bin', ['bottler.sh', 'wine_env_complete.sh'])],
+#     packages=['wine_env_powerline']
+# )
--- wine_env_powerline/wine_env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def bottle(pl, segment_info, create_watcher, *args):
-    return "Bottle Shown"
-    # prefix = segment_info['environ'].get('WINEPREFIX', '')
-    # if not prefix:
-    #     return prefix
-    # return os.path.basename(prefix)
