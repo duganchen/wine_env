36fb3a6ba9eeba454089b6281452d0a14cec4868
Dugan Chen <thedoogster@gmail.com>
2015-03-01 16:14:31 -0800

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+import shutil
+
+shutil.copyfile('bottle-run.sh', 'bottle-run')
 
 setup(
     name='wine_env',
@@ -6,14 +9,12 @@ setup(
     packages=['wine_env'],
     entry_points={
         'console_scripts': [
-            'mkcorkrc = wine_env.mkcorkrc.main',
-            'mkuncorkrc = wine_env.mkuncorkrc.main',
-            'mkrunrc = wine_env.mkrunrc.main'
+            'mkcorkrc = wine_env.mkcorkrc:main',
+            'mkuncorkrc = wine_env.mkuncorkrc:main',
+            'mkrunrc = wine_env.mkrunrc:main'
         ]
     },
-    scripts=(
-        'bottle-run.sh',
-        'bottler.sh',
-        'wine_env_complete.sh'
-    )
+    scripts=('bottle-run',),
+    data_files=[
+        ('/usr/bin', ['bottler.sh', 'wine_env_complete.sh'])]
 )
