f0d2eb1f06101dd79d47d0cdc0875c8a6af13a6a
Dugan Chen <thedoogster@gmail.com>
2016-12-18 20:40:17 -0800

    Full unit tests added

--- test_wine_rc.py
+++ test_wine_rc.py
@@ -1,33 +1,50 @@
 from wine_env import wine_rc
+import os
+
+
+def get_path(filename):
+    directory = os.path.dirname(__file__)
+    return os.path.join(directory, 'test_data', filename)
+
 
 def test_runrc():
-
-    expected = """WINEPREFIX=$HOME/.local/share/wineprefixes/bottle
-export WINEPREFIX
-"""
     rc = wine_rc.RunRC('bottle')
+    with open(get_path('runrc.sh')) as f:
+        expected = f.read()
     assert expected == rc.get_rc()
 
 
 def test_runrc_exe():
-
-    expected = """W=/path/to/wine64-1.4
-
-WINESERVER="$W/bin/wineserver"
-export WINESERVER
-LD_LIBRARY_PATH="$W/lib:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH
-WINELOADER="$W/bin/wine"
-export WINELOADER
-WINESERVERPATH="$W"
-export WINESERVERPATH
-WINEDLLPATH="$W/lib/wine/fakedlls"
-export WINEDLLPATH
-PATH="$W/bin:$PATH"
-export PATH
-WINEPREFIX="$HOME/.local/share/wineprefixes/bottle"
-export WINEPREFIX
-"""
     rc = wine_rc.RunRC('bottle', '/path/to/wine64-1.4/bin/wine')
+    with open(get_path('runrc_exe.sh')) as f:
+        expected = f.read()
     assert expected == rc.get_rc()
 
+
+def test_uncorkc():
+    rc = wine_rc.UncorkRC('bottle')
+    with open(get_path('uncorkrc.sh')) as f:
+        expected = f.read()
+    assert expected == rc.get_rc()
+
+
+def test_uncorkc_exe():
+    rc = wine_rc.UncorkRC('bottle', '/path/to/wine-1.7.3/bin/wine')
+    with open(get_path('uncorkrc_exe.sh')) as f:
+        expected = f.read()
+    assert expected == rc.get_rc()
+
+
+def test_corkrc():
+    rc = wine_rc.CorkRC('bottle')
+    with open(get_path('corkrc.sh')) as f:
+        expected = f.read()
+    assert expected == rc.get_rc()
+
+
+def test_corkrc_exe():
+
+    rc = wine_rc.CorkRC('bottle', '/path/to/wine-1.7.3/bin/wine')
+    with open(get_path('corkrc_exe.sh')) as f:
+        expected = f.read()
+    assert expected == rc.get_rc()
